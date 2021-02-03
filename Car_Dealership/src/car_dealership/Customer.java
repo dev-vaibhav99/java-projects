606f449caf0bc84fdd4c9af3cc0018fa94f0a2a4
Vaibhav Chandrashekhar <dev.vaibhav99@gmail.com>
2021-02-03 19:45:25 +0530

    Creating some more classes to make the program more dynamic

--- Car_Dealership/src/car_dealership/Customer.java
+++ Car_Dealership/src/car_dealership/Customer.java
@@ -1,8 +1,37 @@
 package car_dealership;
 
 public class Customer{
-    public static void main(String[] args) {
-        System.out.println("This is first commit...");
+
+    private String name;
+    private String address;
+    private double cashOnHand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getCashOnHand() {
+        return cashOnHand;
+    }
+
+    public void setCashOnHand(double cashOnHand) {
+        this.cashOnHand = cashOnHand;
+    }
+
+    public void purchaseCar(Vehicle vehicle, Employee employee, boolean finance){
+
     }
 
 }
---
+++ Car_Dealership/src/car_dealership/Dealership.java
@@ -0,0 +1,14 @@
+package car_dealership;
+
+public class Dealership {
+    public static void main(String[] args) {
+
+        Customer cust1 = new Customer();
+        cust1.setName("Rahul");
+        cust1.setAddress("Navi Mumbai");
+        cust1.setCashOnHand(500000);
+
+        cust1.purchaseCar(vehicle, emp, false);
+    }
+
+}
